0c504eb0bd029f81516bdb76797f3aff509c6503
migueln4 <39500754+migueln4@users.noreply.github.com>
2018-11-16 14:18:28 +0100

    Fin del ejercicio

--- js/app.js
+++ js/app.js
@@ -1,17 +1,26 @@
-(function(){
+(function() {
+	var app = angular.module('universidadApp',[]);
+	app.controller('listadoCtrl',['$scope',function($scope) {
+		$scope.listado = ["Miguel Negrillo", "Osete Plastidécor", "Shane McMahon","Fernando Hierro"];
 
-var app = angular.module('universidadApp',[ ]);
+		$scope.listadoLuchadores = {
+			luchadores: [{
+				nombre: "Daniel Bryan",
+				titulo: "WWE Championship",
+				equipo: "Team Hell No"
+			},
+			{nombre: "Seth Rollins",
+				titulo: "Intercontinental Championship",
+				equipo: "The Shield"
+			},
+			{nombre: "Brock Lesnar",
+				titulo: "Universal Championship",
+				equipo: "-"
+			}]
+		}
 
-app.controller('listadoCtrl', ['$scope', function($scope){
-	
-		
 
 
-
-}]);
-
-
-
-
+	}])
 
 })();
